30d27b5bf77033230b646277eb309bba3950f12c
guifroes <guilherme.froes@gmail.com>
2013-04-28 18:49:29 -0300

    Adding function contructors

--- test/functions.js
+++ test/functions.js
@@ -15,6 +15,11 @@ var myNamedFunctionExpression = function namedFunctionExpression(param1, param2,
 	// bla bla
 };
 
+// using the Function constructor
+var myFunctionConstructor1 = Function("param1, param2", "return true;");
+// works like this as well, passing the parameters in separated strings
+var myFunctionConstructor2 = Function("param1", "param2", "return true;");
+
 describe('Functions', function(){
   
   describe('length property', function(){
@@ -24,6 +29,10 @@ describe('Functions', function(){
       assert.equal(2, myFunctionDeclaration.length);
       assert.equal(3, myFunctionExpression.length);
       assert.equal(4, myNamedFunctionExpression.length);
+
+      assert.equal(2, myFunctionConstructor1.length);
+      assert.equal(2, myFunctionConstructor2.length);
+
       assert.equal(2, assert.length);
       assert.equal(0, console.log.length); // this is confusing
 
